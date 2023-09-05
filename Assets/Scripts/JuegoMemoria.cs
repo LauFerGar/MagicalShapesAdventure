c064756b8e796364192c4326cec61ff351e787c1
Laura <lfgarcia91@gmail.com>
2023-09-05 17:45:31 +0200

    Activacion altares con animales
    Se generan animales en los altares, se espera 10 segundos y vuelven al tablero para que el usuario lo ponga en el altar que estaba

--- Assets/Scripts/JuegoMemoria.cs
+++ Assets/Scripts/JuegoMemoria.cs
@@ -6,13 +6,13 @@ using UnityEngine.UI;
 public class JuegoMemoria : MonoBehaviour
 {
     public Vector3[] posicionesAltar;
-    public GameObject animal;
+    public GameObject[] animales;
     public Button contador;
 
     private int tiempoRestante = 11;
     private bool juegoIniciado = false;
     private Text contadorTexto;
-    private Vector3 posicionInicialGallina;
+    private Vector3[] posicionesIniciales;
 
     private void Awake()
     {
@@ -23,10 +23,14 @@ public class JuegoMemoria : MonoBehaviour
     {
 
         contador.onClick.AddListener(ComenzarJuego);
-        posicionInicialGallina = animal.GetComponent<Transform>().localPosition;
 
-        animal.transform.localPosition = posicionesAltar[0];
+        posicionesIniciales = new Vector3[animales.Length];
+        /*for (int i = 0; i<animales.Length; i++)
+        {
+            posicionesIniciales[i] = animales[i].transform.localPosition;
+        }*/
 
+        PosicionarAnimalesAleatoriamente();
     }
 
     public void ComenzarJuego()
@@ -50,7 +54,11 @@ public class JuegoMemoria : MonoBehaviour
         {
             CancelInvoke("ActualizarContador");
             contador.gameObject.SetActive(false);
-            animal.transform.localPosition = posicionInicialGallina;
+
+            for (int i = 0; i<animales.Length; i++)
+            {
+                animales[i].transform.localPosition = posicionesIniciales[i];
+            }
         }
     }
 
@@ -58,10 +66,41 @@ public class JuegoMemoria : MonoBehaviour
     {
         posicionesAltar = new Vector3[]
         {
-            new Vector3(-3.73799992f,0.0909999982f,-3.38000011f),
-            new Vector3(-1.27999997f,0.0909999982f,-3.38000011f),
-            new Vector3(1.16999996f,0.0909999982f,-3.38000011f),
-            new Vector3(3.66000009f,0.0909999982f,-3.38000011f),
+            new Vector3(-3.67000008f,0.0529999994f,-3.48000002f),
+            new Vector3(-1.26999998f,0.0529999994f,-3.48000002f),
+            new Vector3(1.11000001f,0.0529999994f,-3.48000002f),
+            new Vector3(3.54999995f,0.0529999994f,-3.48000002f),
         };
+    }
+
+    private void PosicionarAnimalesAleatoriamente()
+    {
+        List<int> posicionesDisponibles = new List<int>();
+
+        for (int i=0; i<posicionesAltar.Length; i++)
+        {
+            posicionesDisponibles.Add(i);
+        }
+
+        //Posicionamos aleatoriamente a cada animal en uno de los 4 altares
+        for (int i=0; i<animales.Length; i++)
+        {
+            // Guardamos la posición inicial del animal
+            posicionesIniciales[i] = animales[i].transform.localPosition;
+
+            if (posicionesDisponibles.Count ==0)
+            {
+                Debug.LogError("No hay suficientes posiciones disponibles para todos los animales.");
+                break;
+            }
+
+            int indiceAleatorio = Random.Range(0, posicionesDisponibles.Count);
+            int indicePosicion = posicionesDisponibles[indiceAleatorio];
+            posicionesDisponibles.RemoveAt(indiceAleatorio);
+
+            //Asignamos el animal al altar
+            animales[i].transform.localPosition = posicionesAltar[indicePosicion];
+
+        }
     }
 }
